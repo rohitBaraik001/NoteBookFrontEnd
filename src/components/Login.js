83f568039844be867830396a491fc2834ba1daff
rohitBaraik001 <136783344+rohitBaraik001@users.noreply.github.com>
2024-01-10 01:14:35 +0530

    --

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ function Login(props) {
     const navigate = useNavigate();
     const HandleSubmit=async(e)=>{
         e.preventDefault();
-        const response = await fetch("https://inotebookbackend-3y5t.onrender.com/api/auth.js/login",{
+        const response = await fetch("https://inotebookbackend-3y5t.onrender.com/auth.js/login",{
             method:"POST",
             headers:new Headers({
                 "Content-type": "application/json"
